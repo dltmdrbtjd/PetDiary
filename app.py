c0f94da9b8ee81c57ab73f8e49815e01481c9c1e
SeungBinKim <seungbin0508@gmail.com>
2021-06-08 11:24:38 +0900

    Added @app.route for '/api/sign_up'.

--- app.py
+++ app.py
@@ -44,5 +44,11 @@ def api_login():
 def sign_up():
 	return render_template('sign_up.html')
 
+@app.route('/api/sing_up', methods=['POST'])
+def api_sign_up():
+	print('test')
+	return
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
