95441e380822d66f3526a0a2e04a2e90e5f0a2f7
SeungBinKim <seungbin0508@gmail.com>
2021-06-08 20:48:37 +0900

    Changed from form action/submit to fetch API in order to receive result.

--- app.py
+++ app.py
@@ -75,11 +75,11 @@ def api_sign_up():
 	result = request.form
 	_id = request.form['user-id']
 	if isDuplicate(_id):
-		return render_template('sign_up.html')
+		return jsonify({'success': False, 'msg': '중복된 아이디입니다.'})
 	_password = request.form['user-password']
 	_pw_hash = hashlib.sha256(_password.encode('utf-8')).hexdigest()
 	db.user.insert_one({'user_id': _id, 'password': _pw_hash})
-	return render_template('login.html')
+	return jsonify({'success': True, 'msg': '로그인 페이지로 이동합니다.'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
