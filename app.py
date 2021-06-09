b7f41a24e9924a6b51e1daa22911fce8e142126e
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-06-09 17:28:33 +0900

    dd

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ db = client.dbpetdiary
 
 SECRET_KEY = 'PETDIARY'
 
-@ app.route('/')
+
+@app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
@@ -21,6 +22,7 @@ def home():
         return redirect(url_for("login", token_expired="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login"))
+
 
 @app.route('/main')
 def main():
@@ -43,6 +45,7 @@ def login():
     token_expired = request.args.get("token_expired")
     return render_template('login.html', token_expired=token_expired)
 
+
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['id_give']
@@ -62,24 +65,29 @@ def api_login():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
+
 def isDuplicate(_id):
-	if db.user.find_one({'user_id': _id}):
-		return True
-	return False
+    if db.user.find_one({'user_id': _id}):
+        return True
+    return False
+
+
 @app.route('/sign_up')
 def sign_up():
-	return render_template('sign_up.html')
+    return render_template('sign_up.html')
+
 
 @app.route('/api/sign_up', methods=['POST'])
 def api_sign_up():
-	result = request.form
-	_id = request.form['user-id']
-	if isDuplicate(_id):
-		return jsonify({'success': False, 'msg': '중복된 아이디입니다.'})
-	_password = request.form['user-password']
-	_pw_hash = hashlib.sha256(_password.encode('utf-8')).hexdigest()
-	db.user.insert_one({'user_id': _id, 'password': _pw_hash})
-	return jsonify({'success': True, 'msg': '로그인 페이지로 이동합니다.'})
+    result = request.form
+    _id = request.form['user-id']
+    if isDuplicate(_id):
+        return jsonify({'success': False, 'msg': '중복된 아이디입니다.'})
+    _password = request.form['user-password']
+    _pw_hash = hashlib.sha256(_password.encode('utf-8')).hexdigest()
+    db.user.insert_one({'user_id': _id, 'password': _pw_hash})
+    return jsonify({'success': True, 'msg': '로그인 페이지로 이동합니다.'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
